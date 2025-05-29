fcc6fe67d0dc0b77906e0be16ef35b5dd7e4a4ea
Stephane Mfuni <stephuni35@gmail.com>
2025-05-29 05:56:20 +0300

    fix redirect uri from google provider params

--- auth.ts
+++ auth.ts
@@ -19,7 +19,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           prompt: "consent",
           access_type: "offline",
           response_type: "code",
-           redirect_uri: 'http://localhost:3000/api/auth/callback/google'
         },
       },
     }),
